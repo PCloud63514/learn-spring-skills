ce14840e425f407390f6e1ea33458c63b8701216
PCloud63514 <pcloud63514@gmail.com>
2021-12-12 16:51:50 +0900

    gradle 간편 배포하기 추가

---
+++ GradleDistribution/src/main/java/org/pcloud/gradledistribution/GradleDistributionApplication.java
@@ -0,0 +1,14 @@
+package org.pcloud.gradledistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleDistributionApplication.class, args);
+    }
+
+
+}
---
+++ GradleDistribution/src/test/java/org/pcloud/gradledistribution/GradleDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcloud.gradledistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
