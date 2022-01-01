f0fdb4560a36ea5dbe5532ee20c879b047a66979
PCloud63514 <pcloud63514@gmail.com>
2022-01-01 15:48:37 +0900

    update

---
+++ SpringBootCleanExample/src/main/java/org/pcloud/springbootcleanexample/SpringBootCleanExampleApplication.java
@@ -0,0 +1,13 @@
+package org.pcloud.springbootcleanexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCleanExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCleanExampleApplication.class, args);
+    }
+
+}
---
+++ SpringBootCleanExample/src/test/java/org/pcloud/springbootcleanexample/SpringBootCleanExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcloud.springbootcleanexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCleanExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
