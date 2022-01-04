d7aa3c2db8d7e601b337be389f5e3c54e38653c3
PCloud63514 <pcloud63514@gmail.com>
2022-01-04 21:11:26 +0900

    new Project

---
+++ CookieAndSession/src/test/java/org/pcloud/cookieandsession/CookieAndSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcloud.cookieandsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieAndSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
