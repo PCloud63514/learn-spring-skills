ef24d2cfeb917893bf494dd5671cb0dc33d339f2
PCloud63514 <pcloud63514@gmail.com>
2022-02-04 21:36:14 +0900

    User Service 작성 완료

--- EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/service/UserServiceImpl.java
+++ EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/service/UserServiceImpl.java
@@ -1,22 +1,28 @@
 package org.pcloud.eventlistenerexample.user.service;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.core.ApplicationPushBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.pcloud.eventlistenerexample.push.handler.event.PushEvent;
 import org.pcloud.eventlistenerexample.user.domain.User;
 import org.pcloud.eventlistenerexample.user.dto.form.AddUserForm;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
-    private final ApplicationPushBuilder pushBuilder;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Override
     public UUID AddUser(AddUserForm form) {
+        log.info("AddUser 요청");
         User user = User.create(form.getNickName(), form.getPassword());
-        // repository save
+        // repository save가
+        log.info("User 생성 완료!");
+        eventPublisher.publishEvent(new PushEvent(form.getNickName() + "님의 가입을 축하합니다!"));
         return user.getId();
     }
 }
