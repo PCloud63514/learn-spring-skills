a07e349ca23cb514c318f7d8c34ab12521c8672b
PCloud63514 <pcloud63514@gmail.com>
2022-02-04 21:49:54 +0900

    완료

--- EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/controller/UserApi.java
+++ EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/controller/UserApi.java
@@ -5,10 +5,15 @@ import org.pcloud.eventlistenerexample.user.dto.form.AddUserForm;
 import org.pcloud.eventlistenerexample.user.dto.form.JoinUserApiForm;
 import org.pcloud.eventlistenerexample.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("user")
 @RestController
@@ -16,7 +21,7 @@ public class UserApi {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<String> joinUser(JoinUserApiForm form) {
+    public ResponseEntity<String> joinUser(@RequestBody @Valid JoinUserApiForm form) {
         userService.AddUser(new AddUserForm(form.getNickName(), form.getPassword()));
 
         return ResponseEntity.ok("성공");
--- EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/dto/form/JoinUserApiForm.java
+++ EventListenerExample/src/main/java/org/pcloud/eventlistenerexample/user/dto/form/JoinUserApiForm.java
@@ -1,15 +1,18 @@
 package org.pcloud.eventlistenerexample.user.dto.form;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
-@AllArgsConstructor
 @Getter
 public class JoinUserApiForm {
     @NotBlank(message = "닉네임이 입력되지 않았습니다.")
     private final String nickName;
     @NotBlank(message = "패스워드가 입력되지 않았습니다.")
     private final String password;
+
+    public JoinUserApiForm(String nickName, String password) {
+        this.nickName = nickName;
+        this.password = password;
+    }
 }
